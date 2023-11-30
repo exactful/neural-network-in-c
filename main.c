b1643c050053de39561da9ce4869ed7327149a0a
Daniel Cooper <dc@exactful.com>
2023-11-30 21:19:08 +0000

    Added loss per epoch and predictions

--- main.c
+++ main.c
@@ -28,6 +28,9 @@ struct Layer create_layer(int num_neurons, int num_neurons_next_layer);
 float sigmoid(float x);
 float sigmoid_derivative(float x);
 void do_forward_propagation(Layer layers[], int m);
+void compute_node_deltas(Layer layers[], int m);
+void update_weights(Layer layers[], int m);
+void display_weights(Layer layers[], int m);
 
 /* Define the training data ---- */
 
@@ -39,6 +42,9 @@ const float X[2][4] =   {
 const float Y[4] =          {0.0, 1.0, 1.0, 1.0};
 
 const int NUM_TRAINING_EXAMPLES = 4; // Number of training examples in X
+
+/* Define verbose or not; 1 = display additional info, 0 = quiet */
+const int VERBOSE = 0; //
 
 /* Define neural network architecture ---- */
 
@@ -73,64 +79,42 @@ int main() {
 
         for (m=0; m<NUM_TRAINING_EXAMPLES; ++m) {
             
-            printf("*** Epoch %d, training example %d ***\n", epoch, m);
-        
-            // Compute forward pass for training example m
+            // Compute forward pass
             do_forward_propagation(layers, m);
 
-            // Compute loss for this training example
+            // Compute loss
             loss += 0.5 * pow(layers[NUM_LAYERS-1].neurons[0].a - Y[m], 2); // TODO: Hardcoded [0], assumes one output only
 
-            // Compute node deltas dL/dz
-            for (i=NUM_LAYERS-1; i>=0; --i) {
-                // Start at last layer
-                for (j=0; j<layers[i].num_neurons; ++j) {
-                    
-                    if (i == NUM_LAYERS-1) {
-                        // Last layer
-                        layers[i].neurons[j].delta = sigmoid_derivative(layers[i].neurons[j].z) * (layers[i].neurons[j].a - Y[m]); // TODO: Tidy up here for multiple outputs
-                    } else {
-                        // Hidden and input layers
-                        // Multiple output weights for this neuron by node deltas in next layer to right
-                        tmp = 0.0;
+            /* Compute node deltas dL/dz */
+            compute_node_deltas(layers, m);
 
-                        for (k=0; k<layers[i].neurons[j].num_weights; ++k) {
-                            tmp += layers[i].neurons[j].weights[k].w * layers[i+1].neurons[k].delta;
-                        }
-                        layers[i].neurons[j].delta = sigmoid_derivative(layers[i].neurons[j].z) * tmp;
-                    }
-                }
-            }
+            /* Update weights */
+            update_weights(layers, m);
             
-            // Compute weight deltas dz/dw and gradient descent to weights using dL/dz.dz/dw
-            for (i=0; i<NUM_LAYERS; ++i) {
-                for (j=0; j<layers[i].num_neurons; ++j) {
-                    for (k=0; k<layers[i].neurons[j].num_weights; ++k) {
-                        layers[i].neurons[j].weights[k].w = layers[i].neurons[j].weights[k].w - (LEARNING_RATE * layers[i].neurons[j].a * layers[i+1].neurons[k].delta);
-                    }
-                }
-            }
+            /* Display weights */
+            display_weights(layers, m);
 
-            // Display network info
-            for (i=0; i<NUM_LAYERS; ++i) {
-                printf("Layer %d:\n", i);
-                for (j=0; j<layers[i].num_neurons; ++j) {
-                    printf("\tNeuron %d | z: %f a: %f delta: %f weights out: %d [ ", j+1, layers[i].neurons[j].z, layers[i].neurons[j].a, layers[i].neurons[j].delta, layers[i].neurons[j].num_weights);
-                    for (k=0; k<layers[i].neurons[j].num_weights; ++k) {
-                        printf("w%d-%d: %f ", j+1, k+1, layers[i].neurons[j].weights[k].w);
-                    }
-                    printf("]\n");
-                }
-            }
-
-            printf("Loss: %f\n", loss);
-
-            // TODO: Check logic + Add functions + Add comments
+            // TODO: Check logic + Add comments
             // TODO: Check randomisation logic
             // TODO: Add support for multiple outputs: y1, y2 etc... 
 
-
         }
+
+        // Calculate average loss for this epoch
+        loss = loss / NUM_TRAINING_EXAMPLES;
+
+        // Display training info
+        if (epoch % 100 == 0)
+            printf("Epoch %4.0d Loss %8.8f\n", epoch, loss);
+    }
+
+    // Predict using input data
+    for (m=0; m<NUM_TRAINING_EXAMPLES; ++m) {
+            
+        // Compute forward pass
+        do_forward_propagation(layers, m);
+
+        printf("x1: %f x2: %f y: %f prediction: %f\n", X[0][m], X[1][m], Y[m], layers[NUM_LAYERS-1].neurons[0].a);
     }
 }
 
@@ -172,7 +156,6 @@ struct Layer create_layer(int num_neurons, int num_neurons_next_layer) {
 	return layer;
 }
 
-
 // Activiation function
 float sigmoid(float x) {
     return 1.0 / (1.0 + exp(-x));
@@ -204,6 +187,67 @@ void do_forward_propagation(Layer layers[], int m) {
 
                 layers[i].neurons[j].z = weighted_input_to_neuron;
                 layers[i].neurons[j].a = sigmoid(weighted_input_to_neuron);    
+            }
+        }
+    }
+}
+
+/* Compute node deltas dL/dz using training example m */
+void compute_node_deltas(Layer layers[], int m) {
+
+    // For layer i, going from right to left
+    for (int i=NUM_LAYERS-1; i>=0; --i) {
+
+        // For neuron j in layer i
+        for (int j=0; j<layers[i].num_neurons; ++j) {
+            
+            if (i == NUM_LAYERS-1) {
+                // Last layer
+                layers[i].neurons[j].delta = sigmoid_derivative(layers[i].neurons[j].z) * (layers[i].neurons[j].a - Y[m]); // TODO: Tidy up here for multiple outputs
+            } else {
+                // Hidden and input layers
+                // Multiple output weights for this neuron by node deltas in next layer to right
+                float tmp = 0.0;
+                for (int k=0; k<layers[i].neurons[j].num_weights; ++k) {
+                    tmp += layers[i].neurons[j].weights[k].w * layers[i+1].neurons[k].delta;
+                }
+                layers[i].neurons[j].delta = sigmoid_derivative(layers[i].neurons[j].z) * tmp;
+            }
+        }
+    }
+}
+
+void update_weights(Layer layers[], int m) {
+
+    // Compute weight deltas dz/dw and gradient descent to weights using dL/dz.dz/dw
+    // For layer i
+    for (int i=0; i<NUM_LAYERS; ++i) {
+        // For neuron j in layer i
+        for (int j=0; j<layers[i].num_neurons; ++j) {
+            // For weight k in neuron j in layer i
+            for (int k=0; k<layers[i].neurons[j].num_weights; ++k) {
+                layers[i].neurons[j].weights[k].w = layers[i].neurons[j].weights[k].w - (LEARNING_RATE * layers[i].neurons[j].a * layers[i+1].neurons[k].delta);
+            }
+        }
+    }
+}
+
+/* Display neural network weights for training example m*/
+void display_weights(Layer layers[], int m) {
+
+    if (VERBOSE == 1) {
+        printf("Training example %d\n", m);
+        // For layer i
+        for (int i=0; i<NUM_LAYERS; ++i) {
+            printf("Layer %d:\n", i);
+            // For neuron j in layer i
+            for (int j=0; j<layers[i].num_neurons; ++j) {
+                printf("\tNeuron %d | z: %f a: %f delta: %f weights out: %d [ ", j+1, layers[i].neurons[j].z, layers[i].neurons[j].a, layers[i].neurons[j].delta, layers[i].neurons[j].num_weights);
+                // For weight k in neuron j in layer i
+                for (int k=0; k<layers[i].neurons[j].num_weights; ++k) {
+                    printf("w%d-%d: %f ", j+1, k+1, layers[i].neurons[j].weights[k].w);
+                }
+                printf("]\n");
             }
         }
     }
